86f630bd522485093faa31f9821aec5d4c29e3c4
syifaulumam93-art <syifaulumam93@gmail.com>
2026-07-25 06:46:26 +0700

    Update prestasi.js

--- js/prestasi.js
+++ js/prestasi.js
@@ -176,3 +176,40 @@ document
     }
 
 });
+
+/* =====================================================
+   PREVIEW FOTO PRESTASI
+===================================================== */
+
+document
+.getElementById("foto_prestasi")
+.addEventListener("change",function(){
+
+    const file = this.files[0];
+
+    const preview =
+        document.getElementById("previewFotoPrestasi");
+
+    if(!file){
+
+        preview.style.display = "none";
+
+        preview.src = "";
+
+        return;
+
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+
+        preview.src = e.target.result;
+
+        preview.style.display = "block";
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
