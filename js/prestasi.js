c6d2b88ae3e08298917208fa6eedc71b20dfa8bd
syifaulumam93-art <syifaulumam93@gmail.com>
2026-07-25 06:35:02 +0700

    Update prestasi.js

--- js/prestasi.js
+++ js/prestasi.js
@@ -89,3 +89,90 @@ function isiKategoriPrestasi(){
     });
 
 }
+
+/* =====================================================
+   KATEGORI BERUBAH
+===================================================== */
+
+document
+.getElementById("kategori")
+.addEventListener("change",function(){
+
+    const kategori = this.value;
+
+    const tingkat =
+        document.getElementById("tingkat_penghargaan");
+
+    const masa =
+        document.getElementById("masa_berlaku");
+
+    const poin =
+        document.getElementById("jumlah_poin_prestasi");
+
+    // Reset
+    tingkat.innerHTML =
+        '<option value="">Pilih Tingkat Penghargaan</option>';
+
+    masa.value = "";
+
+    poin.value = "";
+
+    if(kategori=="") return;
+
+    // Filter data sesuai kategori
+    const hasil =
+        dataPrestasi.filter(
+            item=>item.kategori===kategori
+        );
+
+    // Isi masa berlaku
+    if(hasil.length>0){
+
+        masa.value =
+            hasil[0].masa_berlaku;
+
+    }
+
+    // Isi dropdown tingkat penghargaan
+    hasil.forEach(item=>{
+
+        tingkat.innerHTML +=
+        `<option value="${item.tingkat_penghargaan}">
+            ${item.tingkat_penghargaan}
+        </option>`;
+
+    });
+
+});
+
+/* =====================================================
+   TINGKAT PENGHARGAAN BERUBAH
+===================================================== */
+
+document
+.getElementById("tingkat_penghargaan")
+.addEventListener("change",function(){
+
+    const kategori =
+        document.getElementById("kategori").value;
+
+    const tingkat =
+        this.value;
+
+    const data =
+        dataPrestasi.find(item=>
+
+            item.kategori===kategori &&
+            item.tingkat_penghargaan===tingkat
+
+        );
+
+    if(data){
+
+        document
+        .getElementById("jumlah_poin_prestasi")
+        .value = data.jumlah_poin;
+
+    }
+
+});
